1eb6e532711b2de9d6ff6709e6295c8026d28b46
Bharatsharma77 <bharatsharma9368@gmail.com>
2021-10-02 14:45:31 +0530

    sign up api

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 //env variable
 require("dotenv").config();
 
+//Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import passport from "passport";
 
-//config
+//configs
 import googleAuthConfig from "./config/google.config";
 
 
@@ -23,6 +24,8 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({extended: false}));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize() );
+zomato.use(passport.session() );
 
 
 //passport configuration
@@ -34,6 +37,8 @@ zomato.use("/auth", Auth);
 
 zomato.get("/", (req,res) => res.json({message: "SetUp Success Yay!!"}));
 
+
 zomato.listen(5000, ()=>
 ConnectDB().then(()=>console.log("Server is up and running"))
 .catch(()=>console.log("DB connection failed")));
+
