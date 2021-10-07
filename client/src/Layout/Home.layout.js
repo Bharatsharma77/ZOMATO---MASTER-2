1b5c45d07fb643ae1650acf280d444dfc925ddd2
Bharatsharma77 <bharatsharma9368@gmail.com>
2021-10-08 01:01:25 +0530

    tailwind & react set up

---
+++ client/src/Layout/Home.layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Components
+import Navbar from "../Components/Navbar";
+ 
+const HomeLayout = (props) => {
+  return <>
+  <Navbar />
+   <div className="container mx-auto lg:px-20">
+   {props.children}
+   </div>
+  </>
+};
+
+export default HomeLayout;
